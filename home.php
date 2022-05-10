cc5332866c672c754192f5acbcfff740cc604e43
Esther Pisano <E.PISANO@students.clark.edu>
2022-05-10 13:08:50 -0700

    trouble with login logout at new user page

--- home.php
+++ home.php
@@ -9,7 +9,11 @@ require 'inc/header.inc.php';
 <a href="login.php" id="login">Login</a>
 <a href="" id="logout">Logout</a>
 
-<h1>Welcome to our great site <?= isset($_SESSION['first_name']) ? $_SESSION['first_name'] : 'New User!' ?></h1>
+<h1>Welcome to Esther's Upload Gallery <?= isset($_SESSION['first_name']) ? $_SESSION['first_name'] : 'New User! Please register and login to use this tool' ?></h1>
+<?php if (!isset($_SESSION['loggedin'])) {
+    die();
+} ?>
+<h2>Upload as many photos as you like! From horses to landscapes</h2>
 <div id="message"></div>
 <script defer src="js/script.js"></script>
 
